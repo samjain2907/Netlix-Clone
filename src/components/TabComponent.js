4e5f1491e32b9f45aaf8db7eafad180651fc1135
Samarth Jain <jain.samarth2907@gmail.com>
2020-04-23 00:08:40 +0530

    Added Tabs

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-
+import TabDoor from "./tabs_nav/TabDoor";
+import TabDevices from "./tabs_nav/TabDevices";
+import TabPrices from "./tabs_nav/TabPrices";
+import "../css/TabsNav.css";
 class TabComponent extends Component {
+  state = {
+    tabIndex: 0
+  };
   render() {
     return (
       <div>
-        <Tabs>
-          <TabList>
-            <Tab>Tab Link 1</Tab>
-            <Tab>Tab Link 2 </Tab>
-            <Tab>Tab Link 3</Tab>
+        <Tabs className="tabs ">
+          <TabList className="tab-nav-container">
+            <Tab className={`${this.state.tabIndex === 0 ? "active" : null}`}>
+              <TabDoor />
+              <p>
+                <strong>
+                  No commitments
+                  <br />
+                  Cancel online at anytime
+                </strong>
+              </p>
+            </Tab>
+            <Tab>
+              <TabDevices />
+              <p style={{ marginTop: "-5.3125rem" }}>
+                <strong>Watch anywhere</strong>
+              </p>
+            </Tab>
+            <Tab>
+              <TabPrices />
+              <p>
+                <strong>Pick your price</strong>
+              </p>
+            </Tab>
           </TabList>
         </Tabs>
       </div>
